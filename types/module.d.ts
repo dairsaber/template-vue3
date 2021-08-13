961946938ed2a6195416f75c9c7e8791b9dcb491
dairsaber <dairsaber@outlook.com>
2021-08-13 10:49:00 +0800

    fix: fix vue文件 ts类型报错的bug

--- types/module.d.ts
+++ types/module.d.ts
@@ -1,10 +1,5 @@
-declare module '*.vue' {
-  import { DefineComponent } from 'vue'
-  const Component: DefineComponent<{}, {}, any>
-  export default Component
-}
-
-declare module 'virtual:*' {
-  const result: any
-  export default result
-}
+// declare module '*.vue' {
+//   import { DefineComponent } from 'vue'
+//   const Component: DefineComponent<{}, {}, any>
+//   export default Component
+// }
