7a9ac5cc570bba0ba1a990284b1681eb5469e846
dairsaber <dairsaber@outlook.com>
2021-08-15 15:51:23 +0800

    feat: 登陆页面基本架子，添加antd的icon

--- components.d.ts
+++ components.d.ts
@@ -4,6 +4,10 @@
 declare module 'vue' {
   export interface GlobalComponents {
     AButton: typeof import('ant-design-vue/es')['Button']
+    AForm: typeof import('ant-design-vue/es')['Form']
+    AFormItem: typeof import('ant-design-vue/es')['FormItem']
+    AIcon: typeof import('ant-design-vue/es')['Icon']
+    AInput: typeof import('ant-design-vue/es')['Input']
   }
 }
 
