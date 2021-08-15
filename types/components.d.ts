1e2b3e7db7a5d03a642f9123f8884d3eb2facdec
dairsaber <dairsaber@outlook.com>
2021-08-15 16:42:35 +0800

    chore: vscode 基本配置 添加 Readme基本说明

--- types/components.d.ts
+++ types/components.d.ts
@@ -2,6 +2,10 @@ declare module 'vue' {
   export interface GlobalComponents {
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+    AButton: typeof import('ant-design-vue/es')['Button']
+    AForm: typeof import('ant-design-vue/es')['Form']
+    AFormItem: typeof import('ant-design-vue/es')['FormItem']
+    AInput: typeof import('ant-design-vue/es')['Input']
   }
 }
 
